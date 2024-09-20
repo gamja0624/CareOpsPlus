68ab1a3c1d5d8d97a9b5582531d6c203a2c25c86
honghong <dlghdwns103@naver.com>
2024-09-20 17:57:04 +0900

    이홍준 - '점검표 항목 수정' 페이지 수정 - 점검표 항목 수정 디테일 페이지 추가

--- src/main/java/himedia/project/careops/controller/admin/SafetyController.java
+++ src/main/java/himedia/project/careops/controller/admin/SafetyController.java
@@ -4,18 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author 이홍준
+ * @editDate 2024-09-20 ~ 
+ */
+
 @Controller
-@RequestMapping("/admin/safety")
-public class SafetyManagementController{
+@RequestMapping("/admin")
+public class SafetyController{
 	
 	@GetMapping("/safety-list")
 	public String smList() {
 		return "admin/safety/safety-list";
 	}
 	
-	@GetMapping("checklist-edit")
+	@GetMapping("/checklist-edit")
 	public String checkListEdit() {
 		return "admin/safety/checklist-edit";
 	}
 	
+//	@GetMapping("/checklist-edit/{ 세부항목번호 }")
+	@GetMapping("/checklist-edit/detail")
+	public String checklistEditDetail() {
+		return "admin/safety/checklist-edit-detail";
+	}
 }
