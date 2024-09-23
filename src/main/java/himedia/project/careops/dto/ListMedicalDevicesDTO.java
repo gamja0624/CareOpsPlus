0cac67effa76350e905785bc92986fa886b63448
printbluee <yhdosa0622@gmail.com>
2024-09-23 17:07:15 +0900

    진혜정 - BE : Dto, entity, service, common, config 패키지 분리하여 생성 - common : Pagenation, PagingButtonInfo 생성 - cofing : BeanConfig 생성 - dto : 의료기기 테이블 생성

---
+++ src/main/java/himedia/project/careops/dto/ListMedicalDevicesDTO.java
@@ -0,0 +1,23 @@
+package himedia.project.careops.dto;
+
+/**
+ * @author 진혜정 
+ * @editDate 2024-09-23
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ListMedicalDevicesDTO {
+
+	
+	
+}
