96f44085a32d517909770e1da6daaa233250bd0e
printbluee <yhdosa0622@gmail.com>
2024-09-24 12:49:44 +0900

    진혜정 - application @EntityScan 삭제 - application.yml 수정 - 의료기기 엔티티, DTO, 리파지토리 수정 - 의료기기 목록 조회 완료, 페이지네이션 수정 예정

--- src/main/java/himedia/project/careops/controller/manager/MedicalController.java
+++ src/main/java/himedia/project/careops/controller/manager/MedicalController.java
@@ -1,20 +1,48 @@
 package himedia.project.careops.controller.manager;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 /**
  * @author 진혜정
  * @editDate 2024-09-19 ~ 
  */
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import himedia.project.careops.common.Pagenation;
+import himedia.project.careops.common.PagingButtonInfo;
+import himedia.project.careops.dto.ListMedicalDevicesDTO;
+import himedia.project.careops.service.MedicalService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/manager/medical-list")
 public class MedicalController {
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	private final MedicalService medicalService;
+	
+	public MedicalController(MedicalService medicalService) {
+		this.medicalService = medicalService;
+	}
 
 	@GetMapping("")
-	public String medicalList() {
+	public String medicalList(@PageableDefault Pageable pageable, Model model) {
+		
+		Page<ListMedicalDevicesDTO> medicalDevicesList = medicalService.findByMedicalDevices(pageable);
+		log.info("medicalDevicesList : {}", medicalDevicesList);
+		PagingButtonInfo paging = Pagenation.getPagingButtonInfo(medicalDevicesList);
+		
+		model.addAttribute("medicalDevicesList", medicalDevicesList);
+		model.addAttribute("paging", paging);
+		
 		return "manager/medical/medical-list";
 	}
 	
