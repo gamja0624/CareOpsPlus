784beb9cb1fbce20bba83a3b91a84d95010d8569
taeyoon0620 <woon0426@gmail.com>
2024-09-25 12:25:46 +0900

    -노태윤 -로그인 기능 구현 -로그인했을 경우 dashboard 로 이름 가지고오기

--- src/main/java/himedia/project/careops/repository/ManagerLoginRepository.java
+++ src/main/java/himedia/project/careops/repository/ManagerLoginRepository.java
@@ -1,4 +1,4 @@
-	package himedia.project.careops.repository;
+package himedia.project.careops.repository;
 
 /*@author 노태윤
 @editDate 2024-09-23~2024-09-24*/
@@ -13,10 +13,10 @@ import himedia.project.careops.entity.Admin;
 import himedia.project.careops.entity.Manager;
 
 @Repository
-public interface AdminLoginRepository extends JpaRepository<Admin, String> {
+public interface ManagerLoginRepository extends JpaRepository<Manager, String> {
 
-    @Query(value = "SELECT * FROM admin WHERE admin_dept_no = ? AND admin_id = ? AND admin_password = ?", nativeQuery = true)
-    Optional<Admin> findByAdminDeptNoAndAdminIdAndAdminPassword(String adminDeptNo, String adminId, String adminPassword);
+    @Query(value = "SELECT * FROM manager WHERE manager_dept_no = ? AND manager_id = ? AND manager_password = ?", nativeQuery = true)
+    Optional<Manager> findByManagerDeptNoAndManagerIdAndManagerPassword(int managerDeptNo, String managerId, String managerPassword);
 }
 // 이것만 써도 오류는안남
 //Optional<Admin> findByAdminDeptNoAndAdminIdAndAdminPassword(String adminDeptNo, String adminId, String adminPassword);
