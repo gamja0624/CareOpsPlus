0e1672202cfaa3414058f55de56092f5c02572d7
honghong <dlghdwns103@naver.com>
2024-09-25 16:06:51 +0900

    이홍준 << 안전관리 >> - 컨트롤러 수정(진행중) - repository 생성 - service 생성

--- src/main/java/himedia/project/careops/controller/admin/SafetyController.java
+++ src/main/java/himedia/project/careops/controller/admin/SafetyController.java
@@ -1,11 +1,19 @@
 package himedia.project.careops.controller.admin;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import himedia.project.careops.dto.SafetyManagementDTO;
+import himedia.project.careops.dto.SafetyManagementListDTO;
+import himedia.project.careops.service.SafetyService;
 
 /**
  * @author 이홍준
@@ -16,8 +24,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class SafetyController{
 	
+	// 로그
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	// 서비스
+	private final SafetyService safetyService;
+	
+	public SafetyController(SafetyService safetyService) {
+		this.safetyService = safetyService;
+	}
+
 	@GetMapping("/safety-list")
-	public String smList() {
+	public String smList(Model model) {
+		/*
+		 * log.info("목록 페이지 실행");
+		 * 
+		 * List<SafetyManagementDTO> safetyResultList =
+		 * safetyService.safetyResultList(); log.info("목록 페이지 safetyResultList 실행");
+		 * List<SafetyManagementListDTO> safetyListAll = safetyService.safetyListAll();
+		 * 
+		 * model.addAttribute("safetyResultList", safetyResultList);
+		 * model.addAttribute("safetyListAll", safetyListAll);
+		 * 
+		 * log.info("safetyResultList >>>{}", safetyResultList);
+		 * log.info("safetyListAll>>>{}", safetyListAll);
+		 */
 		
 		return "admin/safety/safety-list";
 	}
