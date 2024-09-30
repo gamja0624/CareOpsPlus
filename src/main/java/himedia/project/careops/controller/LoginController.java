34cbbaa0d88139115ed1438e689734b4ba72561a
taeyoon0620 <woon0426@gmail.com>
2024-09-30 12:35:57 +0900

    노태윤 - 모달 텍스트 빨간색으로 변경 - 로그아웃하면 세션 제거하기(다시 로그인하게 만들기, 쿠키활용) - login.html에서 확대해도 크기 유지되게 - 비밀번호변경 emoji field 의 중앙에오게배치

--- src/main/java/himedia/project/careops/controller/LoginController.java
+++ src/main/java/himedia/project/careops/controller/LoginController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import himedia.project.careops.service.LoginService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 /**
@@ -86,11 +89,32 @@ public class LoginController {
 			// 로그인 실패시 처리
 			logger.warn("로그인 실패: deptNo={}, userId={}", deptNo, userId);
 			response.put("success", false);
-			response.put("message", loginResult.get("message")); // 에러 메시지 추가
+			response.put("message", loginResult.get("message")); 
 		}
 		// JSON 형식으로 응답데이터를 변환하여 HTTP 200 OK 상태 코드와 함께 응답 반환
 		// JSON 데이터를 클라이언트에게 반환함 클라이언트는 서버로 JSON 형식의 응답을 받게된다.
 		// 이를통해 로그인 성공 여부, 리다이렉트 URL, 에러 메시지 등의 정보 쉽게 처리 가능
 		return ResponseEntity.ok(response);
 	}
+	
+	// 작성자 : 노태윤
+    // 로그아웃 처리
+	// cookie삭제를 통해 로그아웃하면 다시 로그인하게 설정
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        // 쿠키 삭제
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        
+        return "redirect:/";
+    }	
 }
