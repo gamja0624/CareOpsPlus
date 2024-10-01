232eb2a88e9da3d2b4e9dd4c4d182722123ecdf8
honghong <dlghdwns103@naver.com>
2024-10-01 10:13:27 +0900

    이홍준 << 안전관리 >> - thymeleaf URL변경
    << 일일관리보고서 >>
    - 수정페이지 구현 완료

--- src/main/java/himedia/project/careops/controller/admin/DailyReportController.java
+++ src/main/java/himedia/project/careops/controller/admin/DailyReportController.java
@@ -1,31 +1,29 @@
 package himedia.project.careops.controller.admin;
 
-import java.util.Optional;
+/**
+ * @author 이홍준 
+ * @editDate 2024-09-29 ~
+ */
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-
-/**
- * @author 이홍준 
- * @editDate 2024-09-29
- */
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import himedia.project.careops.common.Pagenation;
 import himedia.project.careops.common.PagingButtonInfo;
 import himedia.project.careops.dto.DailyManagementReportDTO;
 import himedia.project.careops.entity.DailyManagementReport;
 import himedia.project.careops.service.DailyReportService;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 @RequestMapping("/admin")
@@ -58,12 +56,36 @@ public class DailyReportController {
 	public String reportDtail(@PathVariable int dmrNo, Model model) {
 		
 		log.info("넘어온 dmrNo {}", dmrNo);
-		DailyManagementReport result = dailyReportService.findByReportNo(dmrNo).get();
+		DailyManagementReportDTO result = dailyReportService.findByReportNo(dmrNo);
 		
 		model.addAttribute("result", result);
 		
 		return "admin/report/report-detail";
 	}
 	
+	// 일일관리 보고서 수정페이지
+	@GetMapping("daily-report-edit/{dmrNo}")
+	public String reportEdit(@PathVariable int dmrNo, Model model) {
+		
+		DailyManagementReportDTO result = dailyReportService.findByReportNo(dmrNo);
+		
+		model.addAttribute("result", result);
+		
+		return "admin/report/report-edit";
+	}
+	
+	// 일일관리 보고서 수정 후 상세 페이지 이동
+	@PostMapping("daily-report-detail/{dmrNo}")
+	public String edit(@PathVariable int dmrNo, DailyManagementReportDTO editReport,  Model model) {
+		
+		log.info("모델로 넘어온 모델 {}", editReport);
+		dailyReportService.editReportDetail(dmrNo, editReport);
+		
+		DailyManagementReportDTO result = dailyReportService.findByReportNo(dmrNo);
+		
+		model.addAttribute("result", result);
+		
+		return "redirect:{dmrNo}" ;
+	}
 	
 }
