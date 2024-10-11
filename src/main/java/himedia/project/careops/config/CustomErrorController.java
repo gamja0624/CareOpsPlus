d803add5b7c3e789b04fa1437d5c1fa7584f5469
printbluee <yhdosa0622@gmail.com>
2024-10-11 09:31:27 +0900

    진혜정 BE - 커스텀 404 처리 기능구현 완료
    FE
    - 담당 기능 구현 css 전체적으로 점검 또는 수정

---
+++ src/main/java/himedia/project/careops/config/CustomErrorController.java
@@ -0,0 +1,26 @@
+package himedia.project.careops.config;
+
+/**
+ * @author 진혜정
+ * @editDate 2024-10-10
+ */
+
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error"; // 기본 에러 경로
+
+    @RequestMapping(PATH)
+    public String error() {
+        return "common/404-notFound"; // 뷰 이름 반환
+    }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+}
